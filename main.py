6e897cc384b7a3fec9fd2bb285f5c0f1f16dfaa8
tokyoxp1 <145002996+tokyoxp1@users.noreply.github.com>
2023-10-05 04:01:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ target_lang = 'zh-TW'
 translator = Translator(target=target_lang)
 
 # 讀取文件
-filename = 'Menu7.rc'
+filename = '1.rc'
 with open(filename, 'r', encoding='utf-16') as f:
     lines = f.readlines()
 
@@ -19,20 +19,27 @@ string_pattern = r'"((?:[^"&]|&[a-zA-Z]+)*?)"'
 # 翻譯字符串
 for i, line in enumerate(lines):
     # 如果行以 BLOCK 或 VALUE 開頭，則跳過翻譯
-    if line.startswith('BLOCK') or line.startswith('VALUE'):
+    if line.strip().startswith('BLOCK') or line.strip().startswith('VALUE'):
         continue
-    matches = re.findall(string_pattern, line)
-    for match in matches:
-        if not re.search(filename_pattern, match):
-            if match:
-                translated = translator.translate(match)
-                if isinstance(translated, str):
-                    translated = translated.replace('＆', '')
-                    lines[i] = lines[i].replace(match, translated)
-                    # 移除 & 符號後面沒有英文字母的情況
-                    if re.search(r'&[^a-zA-Z]', lines[i]):
-                        lines[i] = lines[i].replace('&', '')
-        print(lines[i])
+    # 找到第一個 "" 的位置
+    first_quote = line.find('"')
+    second_quote = line.find('"', first_quote + 1)
+    # 如果找到了 ""，則進行翻譯
+    if first_quote != -1 and second_quote != -1:
+        # 翻譯 "" 中的內容
+        to_translate = line[first_quote+1:second_quote]
+        # 如果 "" 中的內容不是檔案名稱，則進行翻譯
+        if not re.search(filename_pattern, to_translate):
+            translated_str = translator.translate(to_translate)
+            # 將翻譯後的內容替換回原始字符串中
+            if isinstance(translated_str, str):
+                translated_str = translated_str.replace('＆', '')
+                line = line[:first_quote+1] + translated_str + line[second_quote:]
+                # 移除 & 符號後面沒有英文字母的情況
+                if re.search(r'&[^a-zA-Z]', line):
+                    line = line.replace('&', '')
+        lines[i] = line
+    print(line)
 
 # COPY原始文件filename，存為 .bak
 import shutil
